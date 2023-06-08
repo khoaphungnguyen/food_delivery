30d3f483ba4c607d4b74f44badd2a85561ca88ff
Rex Phung <khoatuanphungnguyen@gmail.com>
2023-06-08 02:23:09 -0700

    Add Gin library for developing CRUD APIs in Go to handle routes and HTTP requests

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 type Restaurant struct {
-	Id   int    `json:"id" gorm:"column:id;"` //tag
+	Id   int    `json:"id" gorm:"column:id;"`
 	Name string `json:"name" gorm:"column:name;"`
 	Addr string `json:"addr" gorm:"column:addr;"`
 }
@@ -29,37 +32,109 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	dsn := os.Getenv("MYSQL_SRC")
+	// db connection
+	dsn := os.Getenv("MYSQL_SRC") //"user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Println("Failed to open")
-	}
-
-	// Create a new restaurant
-	newRestaurant := Restaurant{Name: "American Food", Addr: "2 Story Road"}
-	if err := db.Create(&newRestaurant).Error; err != nil {
-		log.Println("Failed to create")
-	}
-	log.Println("New Id", newRestaurant.Id)
-
-	// Get a restaurant by ID from the database
-	var myRestaurant Restaurant
-	if err := db.Where("id = ?", 1).First(&myRestaurant).Error; err != nil {
 		log.Println(err)
 	}
-	log.Println(myRestaurant)
 
-	// Update restaurant's info by ID from the database
-	newName := "Korean Food"
-	updateData := RestaurantUpdate{Name: &newName}
-	if err := db.Where("id = ?", 1).Updates(&updateData).Error; err != nil {
-		log.Println(err)
-	}
-	log.Println("Updated Restaurant:", myRestaurant)
+	// Log all SQL statements into the console
+	db.Debug()
 
-	// Delete restaurant's info by ID from the database
-	if err := db.Table(Restaurant{}.TableName()).Where("id = ?", 7).Delete(nil).Error; err != nil {
-		log.Println(err)
-	}
+	// Use Gin to define routes
+	r := gin.Default()
+	v1 := r.Group("/v1")
+
+	// register routes here
+	restaurants := v1.Group("/restaurants")
+
+	// CREATE API - Create a new restaurant
+	restaurants.POST("/", func(c *gin.Context) {
+		var data Restaurant
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		db.Create(&data)
+		c.JSON(http.StatusCreated, data)
+	})
+
+	// GET/READ API - Get a restaurant by ID from the database
+	restaurants.GET("/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		var data Restaurant
+		db.Where("id = ?", id).First(&data)
+		c.JSON(http.StatusOK, data)
+
+	})
+	// GET/READ All API - Get all restaurants with page and limit parameters
+	restaurants.GET("/", func(c *gin.Context) {
+		type Paging struct {
+			Page  int `json:"page" form:"page"`
+			Limit int `json:"limit" form:"limit"`
+		}
+		var pagingData Paging
+		if err := c.ShouldBind(&pagingData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		if pagingData.Page < 1 {
+			pagingData.Page = 1
+		}
+		if pagingData.Limit < 1 {
+			pagingData.Limit = 10
+		}
+		var data []Restaurant
+		db.Offset((pagingData.Page - 1) * pagingData.Limit).Order("id desc").Limit(pagingData.Limit).Find(&data)
+		c.JSON(http.StatusOK, data)
+
+	})
+	// UPDATE API - Update restaurant's info by ID
+	restaurants.PATCH("/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		var data RestaurantUpdate
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		db.Where("id =?", id).Updates(&data)
+		c.JSON(http.StatusOK, data)
+	})
+
+	// DELETE API Delete restaurant's info by ID from the database
+	restaurants.DELETE("/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		db.Where("id =?", id).Delete(&Restaurant{})
+		c.JSON(http.StatusOK, gin.H{
+			"message": "deleted",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 }
